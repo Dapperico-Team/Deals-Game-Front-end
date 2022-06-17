da7b59085bf45cd24f76320f12772b0017ad5d34
MJ Fattahi <mjfatahi80@gmail.com>
2022-06-18 00:06:50 +0430

    nobody

--- src/components/NoBody.jsx
+++ src/components/NoBody.jsx
@@ -1,38 +1,35 @@
 import React from "react";
 import poly from "../asset/noBodyPolygon.svg";
-import Image from "../asset/about.png";
+import Image from "../asset/nobody.png";
 
 const AboutUs = () => {
   return (
-    <section className="container mx-auto ">
-      <h2 className="mt-[145px] text-center text-[40px] leading-[48px]   font-serif font-extrabold text-accent  ">
-        About Us
-      </h2>
-      <div className="flex flex-col items-center xl:flex-row ">
-        <div className="relative left-0 w-1/2">
-          <img
-            src={poly}
-            alt="polygon"
-            className=" hidden right-0 xl:flex  lg:-mt-16 w-[499.58px] h-[555.4px]"
-          />
-          <img
-            src={Image}
-            alt="about"
-            className=" xl:absolute lg:top-[100px] lg:left-[138px] mt-6 "
-          />
-        </div>
+    <section className="container">
+      <div className="relative">
+        <img
+          src={poly}
+          alt="polygon"
+          className="absolute right-0 hidden xl:block "
+        />
+        <img
+          src={Image}
+          alt="nobody  "
+          className="flex xl:right-0 xl:absolute xl:top-48 "
+        />
+      </div>
+      <div className="flex flex-col items-start p-6 xl:ml-[145px] ">
+        <h2 className="xl:mt-[145px] text-left text-[40px] leading-[48px]   font-serif font-extrabold text-accent  ">
+          What happens if nobody wins?
+        </h2>
 
-        <div className="lg:w-[535.15px] lg:h-[196px] ">
-          <p className=" ml-2 p-8  text-left xl:-ml-12  font-sans font-normal lg:text-[24px] lg:leading-[28.44px] text-primary   ">
-            Deals game gives lottery lovers a chance to win a portion from Deals
-            game pool.
-            <br /> It is based on a smart contract and blockchain
-            <br />
-            technology. Each game has many lucky winners. You can be one of
-            them. It's easy, <br /> Just connect your wallet and participate in
-            the lottery with BUSD or BNB.
-          </p>
-        </div>
+        <p className=" mt-[72px] text-left  xl:w-1/2  font-sans font-normal lg:text-[24px] lg:leading-[28.44px] text-primary   ">
+          If we do not have any winners in any of the matches, 50% of the money
+          that is not won in the matches, is divided between the winners from
+          all the other Matches of the same campaign, and the remaining goes to
+          another pool that is used by the utility token of the website called
+          DG Point. Then you can try your luck in another lottery by airdrop and
+          a free ticket.
+        </p>
       </div>
     </section>
   );
