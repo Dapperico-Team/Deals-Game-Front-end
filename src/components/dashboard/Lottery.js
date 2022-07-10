a9cefbd251f76c153af79c18e2f2cfdbc6500bf4
MJ Fattahi <mjfatahi80@gmail.com>
2022-07-10 14:31:40 +0430

    dashboard

---
+++ src/components/dashboard/Lottery.js
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+
+const Lottery = () => {
+  const [price, setPrice] = useState(0);
+  const [maxTickets, setMaxTickets] = useState(0);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [method, setMethod] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(price, maxTickets, startTime, endTime, method);
+  };
+
+  return (
+    <div className="mt-6">
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Lottery
+            </h3>
+            <p className="mt-1 text-sm text-gray-600">
+              This information will be displayed publicly so be careful what you
+              share.
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <form onSubmit={handleSubmit} method="POST">
+            <div className="shadow sm:rounded-md sm:overflow-hidden">
+              <div className="px-4 py-5 space-y-6 bg-white sm:p-6">
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3 sm:col-span-2">
+                    <label
+                      htmlFor="price"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Price
+                    </label>
+                    <div className="relative mt-1 rounded-md shadow-sm">
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        {/* <span className="text-gray-500 sm:text-sm">wei</span> */}
+                      </div>
+                      <input
+                        type="text"
+                        name="price"
+                        id="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                        placeholder="0.00"
+                        aria-describedby="price-currency"
+                      />
+                      <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                        <span
+                          className="text-gray-500 sm:text-sm"
+                          id="price-currency"
+                        >
+                          WEI
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="MaxTickets"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Max Tickets
+                  </label>
+                  <div className="relative mt-1 rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                    <input
+                      type="number"
+                      value={maxTickets}
+                      onChange={(e) => setMaxTickets(e.target.value)}
+                      name="MaxTickets"
+                      id="MaxTickets"
+                      className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                      placeholder="0"
+                      aria-describedby="MaxTickets"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="startTime"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Start Time
+                  </label>
+                  <div className="relative mt-1 rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                    <input
+                      value={startTime}
+                      onChange={(e) => setStartTime(e.target.value)}
+                      type="date"
+                      name="startTime"
+                      id="startTime"
+                      className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                      placeholder="0"
+                      aria-describedby="startTime"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="endTime"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    End Time
+                  </label>
+                  <div className="relative mt-1 rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                    <input
+                      type="date"
+                      value={endTime}
+                      onChange={(e) => setEndTime(e.target.value)}
+                      name="endTime"
+                      id="endTime"
+                      className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                      placeholder="0"
+                      aria-describedby="endTime"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="method"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Payment Method
+                  </label>
+                  <select
+                    onChange={(e) => setMethod(e.target.value)}
+                    id="method"
+                    name="method"
+                    className="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    defaultValue="Canada"
+                  >
+                    <option value={"BNB"}>BNB</option>
+                    <option value={"BUSD"}>BUSD</option>
+                  </select>
+                </div>
+              </div>
+              <div className="px-4 py-3 text-right bg-gray-50 sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lottery;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,29 @@ import "@fontsource/rubik/500.css";
 import "@fontsource/rubik/400.css";
 import "@fontsource/rubik/700.css";
 import "@rainbow-me/rainbowkit/styles.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Dashboard from "./components/dashboard/Admin";
+import Lottery from "./components/dashboard/Lottery";
+import Price from "./components/dashboard/Price";
+import Status from "./components/dashboard/Status";
+import Validate from "./components/dashboard/Validate";
+import Compelete from "./components/dashboard/Complete";
+import Calculate from "./components/dashboard/Calculate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route path="lottery" element={<Lottery />} />
+        <Route path="status" element={<Status />} />
+        <Route path="price" element={<Price />} />
+        <Route path="validate" element={<Validate />} />
+        <Route path="compelete" element={<Compelete />} />
+        <Route path="calculate" element={<Calculate />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
